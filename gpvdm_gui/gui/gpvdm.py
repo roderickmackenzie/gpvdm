734b061fa65bac0b232d1917f683d184229c7999
Roderick MacKenzie <r.c.i.mackenzie@googlemail.com>
2021-07-19 10:22:07 +0100

    test

--- gpvdm_gui/gui/gpvdm.py
+++ gpvdm_gui/gui/gpvdm.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# 
+#   
 #   General-purpose Photovoltaic Device Model - a drift diffusion base/Shockley-Read-Hall
 #   model for 1st, 2nd and 3rd generation solar cells.
 #   Copyright (C) 2012-2017 Roderick C. I. MacKenzie r.c.i.mackenzie at googlemail.com
